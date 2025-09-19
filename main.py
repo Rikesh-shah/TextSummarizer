3f2f6e0224c5407058d46fe7332eeea10f56cd73
Rikesh-shah <rikeshshah2075@gmail.com>
2025-09-19 22:12:15 +0545

    model trainer

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ except Exception as e:
     raise e
 
 
-# STAGE_NAME = "Model Trainer stage"
+STAGE_NAME = "Model Trainer stage"
 
-# try:
-#     logger.info(f"stage {STAGE_NAME} initiated")
-#     model_trainer_pipeline = ModelTrainerTrainingPipeline()
-#     model_trainer_pipeline.initiate_model_trainer()
-#     logger.info(f"stage {STAGE_NAME} completed")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline = ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 # STAGE_NAME = "Model Trainer stage"
